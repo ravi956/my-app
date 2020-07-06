075158a36dbfb788463842e4ae2d5affaeb51534
Ravi Kumar <ravikumar956078@gmail.com>
2020-07-06 12:53:13 +0530

    Making an interactive component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value :null,
+        }
+    }
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        <button className="square" 
+        onClick={() => this.setState({value: 'X'}) }
+        >
+          {this.state.value}
         </button>
       );
     }
@@ -14,7 +23,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square />;
     }
   
     render() {
