e50fb56a5e62c01d86a982f18d0449e42709f855
Ravi Kumar <ravikumar956078@gmail.com>
2020-07-21 15:07:14 +0530

    Updates

---
+++ precache-manifest.bb558ea7599b3a330379c5a228a876a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175eeac560b2e357651f0bf4a981f2ea",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "87d81917bf8b0033b0cd",
+    "url": "/my-app/static/css/main.7b8c0a23.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/my-app/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d81917bf8b0033b0cd",
+    "url": "/my-app/static/js/main.825d9c5b.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
